7a281abd0538307fd7e40be187e522a48dc23778
Douglas Maxwell <dsm1015@sru.edu>
2022-04-26 17:25:59 -0400

    move prints

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -53,14 +53,12 @@ except Exception as E:
 while True:
     raw_data, addr = s.recvfrom(65535)  # place data into string - raw_data
     eth = ethernet_head(raw_data)  # Use ethernet_head function to parse the ethernet header of the data
-
-    print('\nEthernet Frame:')
-    print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1], eth[2]))
     if eth[2] == 8:
         ipv4 = ipv4_head(eth[3])
-        print('\t - ' + 'IPv4 Packet:')
-        print('\t\t - ' + 'Version: {}, Header Length: {}, TTL: {}, '.format(ipv4[0], ipv4[1], ipv4[2]))
-        print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
         if ipv4[4] == '10.0.2.15' and ipv4[5] == '157.240.229.35':
-            print("yuhhhhhhhh")
+            print('\nEthernet Frame:')
+            print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1], eth[2]))
+            print('\t - ' + 'IPv4 Packet:')
+            print('\t\t - ' + 'Version: {}, Header Length: {}, TTL: {}, '.format(ipv4[0], ipv4[1], ipv4[2]))
+            print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4[3], ipv4[4], ipv4[5]))
     print('\n')
