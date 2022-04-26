4842c600a79745673761ad9d4bb2d105b691bb9c
Douglas Maxwell <dsm1015@sru.edu>
2022-04-26 19:15:57 -0400

    Updated menu, added print_packet_info function

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -24,7 +24,7 @@ def ethernet_head(raw_data):
     return dest_mac, src_mac, proto, data
 
 
-# function to parse the IP headers
+# function to parse the IP headers. Returns ipv4 protocol, source IP, and target IP
 def ipv4_head(raw_data):
     # use .unpack() to get [8 padding, int(1), int(1), 2 padding, char[4], char[4]]
     ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw_data[:20])
@@ -38,8 +38,17 @@ def ipv4_head(raw_data):
     return proto, src, target
 
 
+# function to print parsed packet information
+def print_packet_info(eth, ipv4):
+    print('\nEthernet Frame:')
+    print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1], eth[2]))
+    print('\t - ' + 'IPv4 Packet:')
+    print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4[0], ipv4[1], ipv4[2]))
+
+
 # main
 print("PACKET SNIFFER")
+# menu for program options
 valid_menu = False
 while not valid_menu:
     user_select = input("Options: \n1. Receive all packets\n2. Filter by source IP\n"
@@ -47,8 +56,10 @@ while not valid_menu:
     if int(user_select) in (1, 2, 3, 4):
         valid_menu = True
 
-user_source_ip = input("Enter source IP: ")
-user_destination_ip = input("Enter destination IP: ")
+if int(user_select) in (2, 4):
+    user_source_ip = input("Enter source IP: ")
+if int(user_select) in (3, 4):
+    user_destination_ip = input("Enter destination IP: ")
 
 try:
     # create INET Raw socket. Parameter: (Family:AF_Packet, Type:RAW, Protocol:ntohs)
@@ -78,8 +89,14 @@ while True:
         ipv4_dest = ipv4[2]
 
         # match for user input (src and dest)
-        if ipv4[4] == user_source_ip and ipv4[5] == user_destination_ip:
-            print('\nEthernet Frame:')
-            print('Destination: {}, Source: {}, Protocol: {}'.format(eth_dest, eth_source, eth_proto))
-            print('\t - ' + 'IPv4 Packet:')
-            print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4_proto, ipv4_source, ipv4_dest))
+        if user_select == '4' and ipv4_source == user_source_ip and ipv4_dest == user_destination_ip:
+            print_packet_info(eth, ipv4)
+        # match for user input (src)
+        elif user_select == '2' and ipv4_source == user_source_ip:
+            print_packet_info(eth, ipv4)
+        # match for user input (dest)
+        elif user_select == '3' and ipv4_dest == user_destination_ip:
+            print_packet_info(eth, ipv4)
+        # else, print all packets
+        else:
+            print_packet_info(eth, ipv4)
